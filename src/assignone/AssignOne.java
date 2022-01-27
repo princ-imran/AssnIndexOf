6fab3669a349178ab3aff950532c5bcfd0850b48
Imran Hoshain <princ.imran@gmail.com>
2022-01-27 20:54:35 +0600

    Array and for loop IndexOf Function

--- src/assignone/AssignOne.java
+++ src/assignone/AssignOne.java
@@ -1,35 +1,19 @@
 package assignone;
 public class AssignOne {
-    public static void main(String[] args) {        
-        String MySentence = "kdkfjdk dfkjdkfjdk [ John 1 ] kjkjkjkdjfkdj [ Jane ] kjkjkdjfd  [ Selim ]";        
-        int endIdx = -1;
-        String subString = MySentence.substring(endIdx + 1);        
+    public static void main(String[] args) {
+        String[] MyArray = {"kdkfjdk dfkjdkfjdk [ John 1 ]", "kjkjkjkdjfkdj [ Jane ]", "kjkj[ Selim ]"};
         System.out.print("[");
-        while(subString.contains("[")) {
-            int startingIdx = subString.indexOf("[");
-            endIdx = subString.indexOf("]");
-            String john = subString.substring(startingIdx, endIdx + 1);
-            john = john.substring(1, john.length() - 1).trim();
-            String [] stringArray=john.split(" ");
-            subString = subString.substring(endIdx + 1);
-                System.out.print(john + ", "); 
-           
-        }
-        System.out.print("]");
-        /*
-        String[] sentence = {"kdkfjdk dfkjdkfjdk [ John 1 ]", "kjkjkjkdjfkdj [ Jane ]", "kjkj[ Selim ]"};
-        System.out.print("[");
-        for( int i = 0; i < sentence.length; i++){
-             String subString = sentence[i];
+        for( int i = 0; i < MyArray.length; i++){
+             String subString = MyArray[i];
             int startingIdx = subString.indexOf("[");            
             int endIdx = subString.indexOf("]");            
             String john = subString.substring(startingIdx, endIdx + 1);
             john = john.substring(1, john.length() - 1).trim();
             if (i > 0)
-            System.out.print(", ");
-            System.out.print(john);
+                System.out.print(", ");
+                System.out.print(john);
          }        
         System.out.print("]");
-       */
+       
     }
 }
